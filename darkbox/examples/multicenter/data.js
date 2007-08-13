d4dff72a57cd1423b2abf8505025565d9087985a
abeyang <abeyang@d5ee3e89-5d37-0410-b7c2-fd8623b19026>
2007-08-13 22:43:50 +0000

    darkbox - initial checkin

---
+++ darkbox/examples/multicenter/data.js
@@ -0,0 +1,22 @@
+{
+	"multicenter": [ // "current": true,
+		{"id": "all_centers", "name": "All Centers", "type": "all", "contacts": 1235204, "admins": 4, "contacts_valid": 98001, "contacts_active": 87531, "date_created": 20050711, "switch_url": 'blah.html'},
+		{"id": "advocacy_issues", "name": "Advocacy/Issues", "type": "center", "contacts": 35622, "admins": 8, "contacts_valid": 8055, "contacts_active": 4716, "date_created": 20030211},
+		{"id": "alabama_affiliate", "name": "Alabama Affiliate", "type": "center", "contacts": 52321, "admins": 13, "contacts_valid": 135, "contacts_active": 47, "date_created": 20030211},
+		{"id": "arizona_affiliate", "name": "Arizona Affiliate", "type": "center", "contacts": 25232, "admins": 13, "contacts_valid": 18000, "contacts_active": 547, "date_created": 20030211},
+		{"id": "california_affiliate", "name": "California Affiliate", "type": "center", "contacts": 71234, "admins": 13, "contacts_valid": 23568, "contacts_active": 22447, "date_created": 20030211},
+		{"id": "dev", "name": "Dev Environment", "type": "center", "contacts": 0, "admins": 0, "contacts_valid": 0, "contacts_active": 0, "date_created": 19831013},
+		{"id": "yada", "name": "Yada", "type": "all", "contacts": 1235204, "admins": 4, "contacts_valid": 98001, "contacts_active": 87531, "date_created": 20050711, "switch_url": 'blah.html'},
+		{"id": "breehaha", "name": "Breehaha", "type": "center", "contacts": 35622, "admins": 8, "contacts_valid": 8055, "contacts_active": 4716, "date_created": 20030211},
+		{"id": "devastating", "name": "Devastating", "type": "center", "contacts": 52321, "admins": 13, "contacts_valid": 135, "contacts_active": 47, "date_created": 20030211},
+		{"id": "jaron", "name": "Jargon", "type": "center", "contacts": 25232, "admins": 13, "contacts_valid": 18000, "contacts_active": 547, "date_created": 20030211},
+		{"id": "yoda", "name": "Yo da man", "type": "center", "contacts": 71234, "admins": 13, "contacts_valid": 23568, "contacts_active": 22447, "date_created": 20030211},
+		{"id": "blahtastic", "name": "Blahtastic Four", "type": "center", "contacts": 1, "admins": 1, "contacts_valid": 1, "contacts_active": 0, "date_created": 19831013},
+		{"id": "brouhaha", "name": "Brouhaha", "type": "all", "contacts": 1235204, "admins": 4, "contacts_valid": 98001, "contacts_active": 87531, "date_created": 20050711, "switch_url": 'blah.html'},
+		{"id": "abeyance", "name": "Abeyance", "type": "center", "contacts": 35622, "admins": 8, "contacts_valid": 8055, "contacts_active": 4716, "date_created": 20030211},
+		{"id": "perchance", "name": "To Sleep Perchance to Dream", "type": "center", "contacts": 52321, "admins": 13, "contacts_valid": 135, "contacts_active": 47, "date_created": 20030211},
+		{"id": "dreams", "name": "What Dreams May Come", "type": "center", "contacts": 25232, "admins": 13, "contacts_valid": 18000, "contacts_active": 547, "date_created": 20030211},
+		{"id": "prank", "name": "Prank", "type": "center", "contacts": 71234, "admins": 13, "contacts_valid": 23568, "contacts_active": 22447, "date_created": 20030211},
+		{"id": "plank", "name": "Arrr Mateys", "type": "center", "contacts": 0, "admins": 0, "contacts_valid": 0, "contacts_active": 0, "date_created": 19831013}
+	]
+}
