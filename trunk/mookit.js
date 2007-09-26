aba7e3744767719933ce930863a16db6bdd0971e
abeyang <abeyang@d5ee3e89-5d37-0410-b7c2-fd8623b19026>
2007-09-26 23:12:52 +0000

    Mookit upgrade

--- trunk/mookit.js
+++ trunk/mookit.js
@@ -7,19 +7,133 @@
  *  Please visit http://code.google.com/p/mookit/ for more details.
 /*--------------------------------------------------------------------------*/
 
-// make Element class more Prototype-esque
 Element.extend({
-        hide: function() {
-			this.setStyle('display', 'none');
-        },
+	popup: function(options) {
+		this.addEvent('click', function(e) {
+			new Event(e).stop();
 
-        show: function() {
-			this.setStyle('display', '');
-        },
+			var defaultoptions = {
+				name: 'new_window',
+				width: 640,
+				height: 480,
+				status: 0,
+				location: 0,
+				menubar: 0,
+				toolbar: 0,
+				resizable: 1,
+				scrollbars: 1
+			};
+		
+			var str = '';
+			options = options || {};
+			options = $H($extend(defaultoptions, options));
+			options.each(function(val, key) {
+				if (str != '') str += ',';
+				str += key + '=' + val;
+			});
+		
+			window.open(this.href, options.name, str);
+		}, this);
+	},
+	
+	// following makes Element class more Prototype-esque:
+    hide: function() {
+            this.setStyle('display', 'none');
+    },
 
-        toggle: function() {
-			(this.getStyle('display') == "none") ? this.show() : this.hide();
-        }
+    show: function() {
+            this.setStyle('display', '');
+    },
+
+    toggle: function() {
+            (this.getStyle('display') == "none") ? this.show() : this.hide();
+    },
+    
+    visible: function() {
+            return (this.getStyle('display') == "none") ? false : true;
+    },
+    
+    hasClassName: function(name) { 
+    		return this.hasClass(name);
+    },
+    
+    immediateDescendants: function() { 
+    		return this.getChildren();
+    }, 
+    
+    ancestors: function () {
+	        var result = [], el = this;
+    		while (el = el.getTag() !== 'html' ? el.getParent() : false) result.push(el);
+	        return $$(result);
+	},
+    
+    down: function(cssRule, index) {
+    		if ((cssRule == null || cssRule == 0) && (index == null || index == 0)) return this.getFirst();        
+    		if (index == null) index = 0;
+
+    		var elems = this.getElements(cssRule);
+     		return elems[index];
+    },
+    
+    up: function(cssRule, index) {
+    		if ((cssRule == null || cssRule == 0) && (index == null || index == 0)) return this.getParent();            		       		
+    		
+    		if (cssRule == null) {
+     			var elems = this.ancestors();
+        		if (index == null) index = 0;     	         			
+     			return elems[index]
+    		} else {
+    			elems = $$(cssRule);
+    			
+        		var parent_el = this.getParent();
+        		var curr_index = 0;
+        		while(parent_el) {        			
+        			if ((index == null || curr_index == index) && elems.contains(parent_el)) {
+        				return parent_el;
+        			} else {
+        				curr_index++;
+        				parent_el = parent_el.getParent();
+        			}
+        		}       			
+    		}        		
+    },
+    
+    previous: function(cssRule, index) {
+    		if (cssRule == null && index == null) return this.getPrevious();
+
+    		// Filter out siblings
+    		var siblings_filtered = this.getParent().getElements(cssRule);
+    		
+    		var previous_sibling = this.getPrevious();
+    		var curr_index = 0;
+    		while(previous_sibling) {        			
+    			if ((index == null || curr_index == index) && siblings_filtered.contains(previous_sibling)) {
+    				return previous_sibling;
+    			} else {
+    				curr_index++;
+    				previous_sibling = previous_sibling.getPrevious();
+    			}
+    		}
+    },   
+            
+    next: function(cssRule, index) {
+    		if (cssRule == null && index == null) return this.getPrevious();
+
+    		// Filter out siblings
+    		var siblings_filtered = this.getParent().getElements(cssRule);
+
+    		var next_sibling = this.getNext();
+    		var curr_index = 0;
+    		while(next_sibling) {        			
+    			if ((index == null || curr_index == index) && siblings_filtered.contains(next_sibling)) {
+    				return next_sibling;
+    			} else {
+    				curr_index++;
+    				next_sibling = next_sibling.getNext();
+    			}
+    		}
+    }
+
 });
 
 String.extend({
